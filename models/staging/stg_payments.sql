906bc19cacd2fc05e8c5d9716cd60cab35b63c4c
Francois Vienneau-Binette <francois.vienneaubinette@novipro.com>
2020-03-05 21:06:47 +0000

    Added sources in schema.yml

--- models/staging/stg_payments.sql
+++ models/staging/stg_payments.sql
@@ -3,4 +3,5 @@ select
     "orderID" as order_id,
     -- Amount is stored as cents, converting it to dollars.
     AMOUNT / 100 as amount
-from raw.stripe.payment
+-- from raw.stripe.payment
+from {{ source('stripe', 'payment') }}
